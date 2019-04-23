28429d10ccaa2fc5a2e4ed948e9ab6193a705e5a
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-23 02:00:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
+
+
 def oxford_comma(array)
-  if array.length==1
-    return array.join
-  elsif array.length==2
-    return array.join(" and ")
-    elsif array.length>2
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
